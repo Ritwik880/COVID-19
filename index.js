3d4d50e084eec511d688ba83b79f3b2050920286
Ritwik Sinha <54446880+Ritwik880@users.noreply.github.com>
2020-05-25 08:02:14 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+function updateMap() {
+    fetch("/data.json")
+        .then(response => response.json())
+        .then(data => {
+            console.log("rsp.data")
+            rsp.data.forEach(element => {
+                latitude = element.latitude;
+                longitude = element.longitude;
+
+                cases = element.infected;
+                if(cases>255)
+                {
+                    color = "rgb(255, 0, 0)";
+
+                }
+                else{
+                    color = `rgb(${cases}, 0, 0)`;
+                }
+
+
+                //Mark on the map
+                new mapboxgl.Marker({
+                    draggable: false,
+                    color: color
+                })
+                    .setLngLat([longitude, latitude])
+                    .addTo(map);
+
+            });
+        })
+
+}
+
+updateMap();
